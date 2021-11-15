6e755a6e90138aaf21cfa2a13db53d0b698da901
Kishor Prins <me@68x.ca>
2021-11-15 14:27:38 -0500

    Added a search function find friends

--- app/models/member.rb
+++ app/models/member.rb
@@ -14,10 +14,46 @@ class Member < ApplicationRecord
      ShortenUrl.perform_later(self.id, self.url)
   end
 
+  # Helper to get all friends ids
+  def all_friends_ids
+    all_friend_ids = first_friendships.pluck(:friend1_id, :friend2_id) + second_friendships.pluck(:friend1_id, :friend2_id)
+    all_friend_ids.flatten!
+    all_friend_ids.reject! { |f_id| f_id == self.id}
+
+    return all_friend_ids
+  end
+
   def friend_links(base_url)
-    friend_ids = MemberFriendship.where('friend1_id = ? OR friend2_id = ?', self.id, self.id).pluck('friend1_id, friend2_id')
-    friend_ids.flatten!
-    friend_ids = friend_ids.select { |friend_id| friend_id != self.id }
+    friend_ids = self.all_friends_ids
     friend_ids.map { |friend_id| "#{base_url}/member/#{friend_id}"}
+  end
+
+  # This will use a slightly modified Depth-first search to find experts
+  # this will be slow due to N+1 queries
+  def find_experts(topic, intial_member, visited_members)
+    # Prevent cycles
+    if visited_members.include?(self.id)
+      return []
+    end
+
+    all_friend_ids = self.all_friends_ids()
+    experts = []
+
+    # This member is a friend of the initial member
+    if intial_member != self.id && !all_friend_ids.include?(intial_member)
+      # Check if any of our headings matches the topic
+      headings = (self.h1 || []) + (self.h2 || []) + (self.h3 || [])
+      am_expert = headings.any? { |h| h.include?(topic) }
+      experts.append(self.id) if am_expert
+    end
+
+    visited_members.append(self.id)
+
+    all_friend_ids.each do |f_id|
+      friends_experts = Member.find_by(id: f_id).find_experts(topic, intial_member, visited_members)
+      experts.concat(friends_experts)
+    end
+
+    return experts
   end
 end
